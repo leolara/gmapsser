8aebf7855167478303ec5db020efbcb36a96f9b8
Leo Lara <leo@leolara.me>
2013-01-14 17:14:11 +0000

    Change function names for more readability

--- gmapsser.js
+++ gmapsser.js
@@ -20,82 +20,82 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 
-function gmaps_result_ser(result) {
+function gmaps_result_serialize(result) {
     return {
         address_components: result.address_components,
         formatted_address: result.formatted_address,
-        geometry: gmaps_geometry_ser(result.geometry),
+        geometry: gmaps_geometry_serialize(result.geometry),
         types: result.types
     }
 }
 
-function gmaps_geometry_ser(geometry) {
+function gmaps_geometry_serialize(geometry) {
     var result = {
         location_type: geometry.location_type
     }
     
     if (typeof(geometry.location) != 'undefined') {
-        result.location = gmaps_latlng_ser(geometry.location);
+        result.location = gmaps_latlng_serialize(geometry.location);
     }
     
     if (typeof(geometry.bounds) != 'undefined') {
-        result.bounds = gmaps_latlngbounds_ser(geometry.bounds);
+        result.bounds = gmaps_latlngbounds_serialize(geometry.bounds);
     }
     
     if (typeof(geometry.viewport) != 'undefined') {
-        result.viewport = gmaps_latlngbounds_ser(geometry.viewport);
+        result.viewport = gmaps_latlngbounds_serialize(geometry.viewport);
     }
     
     return result;
 }
 
-function gmaps_latlng_ser(latlng) {
+function gmaps_latlng_serialize(latlng) {
     return {
         lat: latlng.lat(),
         lng: latlng.lng()
     }
 }
 
-function gmaps_latlngbounds_ser(bounds) {
+function gmaps_latlngbounds_serialize(bounds) {
     return {
-        sw: gmaps_latlng_ser(bounds.getSouthWest()),
-        ne: gmaps_latlng_ser(bounds.getNorthEast())
+        sw: gmaps_latlng_serialize(bounds.getSouthWest()),
+        ne: gmaps_latlng_serialize(bounds.getNorthEast())
     }
 }
 
-function gmaps_result_deser(result) {
+function gmaps_result_unserialize(result) {
     return {
         address_components: result.address_components,
         formatted_address: result.formatted_address,
-        geometry: gmaps_geometry_deser(result.geometry),
+        geometry: gmaps_geometry_unserialize(result.geometry),
         types: result.types
     }
 }
 
-function gmaps_geometry_deser(geometry) {
+function gmaps_geometry_unserialize(geometry) {
     var result = {
         location_type: geometry.location_type,
     }
     
     if (typeof(geometry.location) != 'undefined') {
-        result.location = gmaps_latlng_deser(geometry.location);
+        result.location = gmaps_latlng_unserialize(geometry.location);
     }
     
     if (typeof(geometry.bounds) != 'undefined') {
-        result.bounds = gmaps_latlngbounds_deser(geometry.bounds);
+        result.bounds = gmaps_latlngbounds_unserialize(geometry.bounds);
     }
     
     if (typeof(geometry.viewport) != 'undefined') {
-        result.viewport = gmaps_latlngbounds_deser(geometry.viewport);
+        result.viewport = gmaps_latlngbounds_unserialize(geometry.viewport);
     }
     
     return result;
 }
 
-function gmaps_latlng_deser(latlng) {
+function gmaps_latlng_unserialize(latlng) {
     return new google.maps.LatLng(latlng.lat, latlng.lng);
 }
 
-function gmaps_latlngbounds_deser(bounds) {
-    return new google.maps.LatLngBounds(gmaps_latlng_deser(bounds.sw), gmaps_latlng_deser(bounds.ne));
+function gmaps_latlngbounds_unserialize(bounds) {
+    return new google.maps.LatLngBounds(gmaps_latlng_unserialize(bounds.sw), gmaps_latlng_unserialize(bounds.ne));
 }
